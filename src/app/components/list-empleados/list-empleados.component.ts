f7654a9234a85946dce1a0e2b39078486186b5b5
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-02 14:37:45 -0300

    ListEmpleados service

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-list-empleados',
@@ -8,11 +9,26 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list-empleados.component.css']
 })
 export class ListEmpleadosComponent implements OnInit {
-  
-  constructor() { 
+  empleados: any[] = [];
+
+  constructor(private _empleadoService: EmpleadoService) {
   }
 
   ngOnInit(): void {
+    this.getEmpleados()
+  }
+
+  getEmpleados() {
+    this._empleadoService.getEmpleados().subscribe(data => {
+      this.empleados = [];
+      data.forEach((element: any) => {
+        this.empleados.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.empleados);
+    });
   }
 
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +12,9 @@ export class EmpleadoService {
   agregarEmpleado(empleado: any): Promise<any> {
     return this.firestore.collection('empleados').add(empleado);
   }
+
+  getEmpleados(): Observable<any> {
+    return this.firestore.collection('empleados', ref => ref.orderBy('fechaCreacion', 'asc')).snapshotChanges();
+  }
+
 }
